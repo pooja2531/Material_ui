53053101d525f6823b36216c2c5a2d949aa41678
pooja chandra <poojachandra1233@gmail.com>
2024-09-23 23:30:14 -0700

    3rd

--- materia/src/component/BadgeMui.js
+++ materia/src/component/BadgeMui.js
@@ -4,8 +4,24 @@ import EmailIcon from '@mui/icons-material/Email';
 
 function BadgeMui() {
   return (
-    <Stack>
-        <Badge badgeContent={5}>
+    <Stack spacing={2} direction={'row'}>
+        <Badge badgeContent={5} color='primary'>
+         <EmailIcon></EmailIcon>
+        </Badge>
+
+        <Badge badgeContent={160} color='primary' max={150}>
+         <EmailIcon></EmailIcon>
+        </Badge>
+
+        <Badge badgeContent={0} color='primary' showZero >
+         <EmailIcon></EmailIcon>
+        </Badge>
+
+        <Badge badgeContent={0} color='secondary' showZero >
+         <EmailIcon></EmailIcon>
+        </Badge>
+
+        <Badge variant='dot' color='primary' showZero >
          <EmailIcon></EmailIcon>
         </Badge>
        
